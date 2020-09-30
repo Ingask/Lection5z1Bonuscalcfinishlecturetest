a26716de635e51b9cb784c69f33249192339c8e5
Inga Oberemchuk <inga_oberemchuk@mail.ru>
2020-09-30 20:17:59 +0300

    Java-code added

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        //подготавливаем данные
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        //вызываем целевой метод
+        long actual = service.calculate(amount, registered);
+
+        //производим проверку
+        //если  TRUE - то Pass
+        //если false - то Fail
+        boolean passed = expected == actual;
+
+        //выводим результат
+        System.out.println(passed);
+    }
+}
